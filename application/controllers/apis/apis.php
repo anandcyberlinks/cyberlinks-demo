e7fadc1bc209ae60e2c8235df6672f8427c32843
anand.cyberlinks <anand.cyberlinks@gmail.com>
2014-11-05 06:54:41 +0000

    change by anand

--- application/controllers/apis/apis.php
+++ application/controllers/apis/apis.php
@@ -3,6 +3,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH.'/libraries/REST_Controller.php';
 
 class Apis extends REST_Controller{
-
     
+    public $app = '';
+    
+    /***
+     * at : Application token
+     */ 
+    function __construct(){
+        parent::__construct();
+        $qString = $this->get();
+        if(isset($qString['at']) && $qString['at'] != ''){
+            $flag = $this->validrequest($qString['at']);
+            if($flag){
+                
+            }else{
+                $this->response(array('error'=>'Invalid Token'));
+            }
+        }else{
+            if($this->uri->segment(2) == 'users' && $this->uri->segment(3) == 'applogin'){
+                return true;
+            }else{
+                $this->response(array('error'=>'Invalid Request'));
+            }
+        }
+    }
+    
+    function validrequest($token){
+        $this->db->select('*');
+        $this->db->where('token',$token);
+        $resultset = $this->db->get('users')->result();
+        if(isset($resultset['0']->id) && $resultset['0']->id > 0){
+            $this->app = $resultset[0];
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    function response($data){
+        $type = isset($_GET['type']) && $_GET['type'] != '' ? $_GET['type'] : 'json';
+        switch($type){
+            case 'json' :
+                print json_encode($data);
+                break;
+            case 'xml' :
+                print $this->array2xml($data);
+                break;
+        }
+        exit;
+    }
+    
+    function array2xml($array, $xml = false){
+        if($xml === false){
+            $xml = new SimpleXMLElement('<result/>');
+        }
+        foreach($array as $key => $value){
+            if(is_array($value)){
+                $this->array2xml($value, $xml->addChild($key));
+            }else{
+                $xml->addChild($key, $value);
+            }
+        }
+        return $xml->asXML();
+    }
 }
--- application/controllers/apis/users.php
+++ application/controllers/apis/users.php
@@ -1,18 +1,25 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-require APPPATH.'/libraries/REST_Controller.php';
+require APPPATH.'controllers/apis/apis.php';
 
-class Users extends REST_Controller{
+class Users extends Apis{
     
     //Application user
     function applogin_post(){
         $data = $this->post();
         $response = array();
         if(isset($data['username']) && $data['username'] != '' && isset($data['password']) && $data['password'] != ''){
-            
+            $query = sprintf('select * from users where username  = "%s" AND password = "%s" ',$data['username'],md5($data['password']));
+            $dataset = $this->db->query($query)->result();
+            $dataset = reset($dataset);
+            if(isset($dataset->id)){
+                $response['token'] = $dataset->token;
+            }else{
+                $response['error'] = 'Username and password not valid';
+            }
         }else{
             $response['error'] = 'Username and password not valid';
         }
-        echo json_encode($response);
+        $this->response($response);
         exit;
     }
 }
