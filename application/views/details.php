4f97939b86ddff89b088477d838b1fec539b238f
shekher.cyberlinks <shekher.cyberlinks@gmail.com>
2015-03-20 06:33:33 +0000

    test for m3u8 ad in mobile

--- application/views/details.php
+++ application/views/details.php
@@ -314,7 +314,7 @@ autostart: 1,
 		//'skipoffset':5,
 		//tag: "<?php //echo ($row['ad_type'] != 'External' ? base_url():'') . $row['vast_file']; ?>?<?php //echo $row['ads_id']?>/<?php //echo $user_id?>/<?php //echo $row['uid']?>"
 		//tag: "<?php echo $row['vast_file']?>?<?php echo $row['ads_id']?>/<?php echo $user_id?>/<?php echo $row['uid']?>/<?php echo ($offset==0 ? 'pre':'mid')?>"
-		tag: "http://localhost/multitvfinal-demo/assets/upload/ads/vast/vast1.xml"
+		tag: "http://54.179.170.143/multitvfinal/assets/upload/ads/vast/vast1.xml"
 		//tag: "http://54.179.170.143/multitvfinal/assets/upload/ads/vast/d53be859b9314be0885eda3794321e05.xml"
 		},
 	   <?php $i++;
