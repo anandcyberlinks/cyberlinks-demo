ae83535593969f1f1d5feb1effb956282d181cb5
shekher.cyberlinks <shekher.cyberlinks@gmail.com>
2015-04-04 11:29:35 +0000

    debug print

--- assets/js/analytics.js
+++ assets/js/analytics.js
@@ -58,8 +58,8 @@ function play() {
                 play: '1',		
 		},		
 		cache: false,
-		type: "get",
-		dataType: 'jsonp',
+		type: "post",
+		//dataType: 'jsonp',
 		success: function(data,textStatus,jqXHR){
 		alert(data);
 		}
