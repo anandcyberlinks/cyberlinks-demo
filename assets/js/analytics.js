2d8adbc7bd0d32252a6d90eec3919f68c74d2543
shekher.cyberlinks <shekher.cyberlinks@gmail.com>
2015-04-04 09:16:48 +0000

    analytics gp

---
+++ assets/js/analytics.js
@@ -0,0 +1,142 @@
+var id=0;
+var duration;  
+var pos=0;
+var ad_duration =0;
+var ad_id =0;
+
+function play() {
+	if(id ==0){
+		$.ajax({
+			url: "http://localhost/multitvfinal-demo/api/analytics/play",
+			data: {		
+		       // user_id:'',
+		        content_id:'92',
+		        content_provider:'59',
+		        play: '1'			
+		       },
+		       cache: false,
+		       type: "POST",
+		       dataType: 'json'	
+		})
+		.done(function(data){
+			//$('#analytics_id').val(data);	
+			//$.cookie("a_id", data);
+			id = data;
+		});
+	}
+    }
+    
+    function pause(duration){	
+
+	if(id >0){
+		//alert(analytics_id);
+		$.ajax({
+		    url: "http://localhost/multitvfinal-demo/api/analytics/pause",
+		    data: {
+			id: id,
+		        watched_time: duration,
+		        pause: '1'
+			},
+		        cache: false,
+		        type: "POST"            
+		})
+		.done(function(data){
+		    
+		});
+	}
+    }
+
+	function playAds(tag) {
+	$.ajax({
+		url: "http://localhost/multitvfinal-demo/api/analytics/playads",
+		data:{
+		tag:tag,
+		broadcaster:'59',		
+                play: '1',		
+		},
+		cache: false,
+		type: "post"
+	})
+	.done(function(data){
+		ad_id = data;
+	});
+   }
+
+//-- ads completed --//
+   function completeAds(ad_duration) {
+        $.ajax({
+            url: "http://localhost/multitvfinal-demo/api/analytics/ads_complete",
+            data: {
+                id: ad_id,
+                watched_time: ad_duration,
+                complete: '1',
+		pause: 0
+                },
+                cache: false,
+                type: "POST"            
+        })
+        .done(function(data){
+          //  if (data > 0) {
+	//	$('#is_complete').val('1');
+	   // }
+        });
+    }
+    
+jwplayer().onPause(function () {         
+            if(pos >0){
+                pos = parseInt(this.getPosition())-pos1;
+                pos1 = pos1+pos;
+            }else{
+                pos = parseInt(this.getPosition());
+                pos1=pos;
+            }
+            pause(pos1);           
+            }
+    );
+
+    jwplayer().onPlay(function (){    
+		play();  
+        });
+  
+jwplayer().onAdPlay(function (event) {	
+	 //console.log(event.tag);
+	var tag = event.tag;
+	playAds(tag);
+});
+
+jwplayer().onAdPause(function (event) {
+	pauseAds(ad_duration);
+});
+
+jwplayer().onAdTime(function(event) {
+  ad_duration = Math.round(event.position);
+  //if (ad_duration >= 1.0 && ad_duration < 2 ) {
+	//jwplayer().setMute(false);
+	//console.log('123stop_ad');	
+	//window.location.href="<?php echo $uri;?>#1234_ad"
+  //}
+//  var flag =/pre/i.test(event.tag);
+/*
+  if (id==56 && flag==false) {
+	switch_ad_skip();
+  }*/
+});
+
+//--- advertising analytics ---//
+jwplayer().onAdComplete(function(event){			
+	  /*var flag =/pre/i.test(event.tag);
+	  if (flag==true) {
+		 $('#flagad').val('0');
+		  $('#tag').val('');
+	  }
+	jwplayer().setMute(true);
+	console.log('123start_ad');
+	window.location.href="<?php echo $uri;?>#123_ad"
+	*/
+	console.log(ad_duration);
+	completeAds(ad_duration);
+}); 
+   /*
+  jwplayer().onAdSkipped(function(event) {
+	skipAds(ad_duration);
+ });*/
